68cf54447c6be6dadbe72ce038d82c0a0b747223
Daniil Kirichenco <mannol.2015@mail.ru>
2024-03-27 15:39:04 +0300

    chenged numbers 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
        int maxNumber = max(250, 80 );
-        System.out.println(Number);
+        System.out.println(max Number);
 
     }
 
